77e55a8f2091bf492ba65ff1c43807c41df76c79
Mark Johnson <mj464@cam.ac.uk>
2021-12-01 08:55:36 +0000

    Day 1

---
+++ 1/1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main(void){
+  std::vector<int> vec;
+  
+  std::ifstream infile("input");
+  if (infile.is_open()){
+    std::string line;
+    while (getline(infile, line)){
+      vec.push_back(std::stoi(line));
+    }
+  }
+  infile.close();
+  
+  int count = 0;
+  for (int i=0; i+1<vec.size(); ++i){
+    if (vec[i+1]>vec[i]){
+      ++count;
+    }
+  }
+  
+  std::cout << count << std::endl;
+}
